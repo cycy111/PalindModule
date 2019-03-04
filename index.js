ca4aa6ed0f86c29df2432a40c83fe6116e354382
cwhycwhy <280587122@qq.com>
2019-03-04 01:02:46 -0800

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
-// Reverses a string.
-function reverse(string) {
-  return string.split("").reverse().join("");
+module.exports = Phrase;
+
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
 }
-
-// Returns true for a palindrome, false otherwise.
-function palindrome(string) {
-   let processedContent = string.toLowerCase();
-     return processedContent === reverse(processedContent);
-     }
-
 // Defines a Phrase object.
 function Phrase(content) {
-    this.content = content;
-
-    this.processor = function(string) {
-      return string.toLowerCase();
-    }
-    // Returns content processed for palindrome testing.
-    this.processedContent = function processedContent() {
-      return this.processor(this.content);
-    }
-	  // Returns true if the phrase is a palindrome, false otherwise.
-	  this.palindrome = function palindrome() {
-		      return processedContent === reverse(this.processedContent());
-		}
-}
-
-// Defines a TranslatedPhrase object.
-function TranslatedPhrase(content, translation) {
   this.content = content;
-  this.translation = translation;
-  // Returns translation processed for palindrome testing.
+  this.processor = function(string) {
+    return string.toLowerCase();
+  }
+  // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.processor(this.translation);
+    return this.letters().toLowerCase();
+  }
+  // Returns the letters in the content.
+  // For example:
+  //   new Phrase("Hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+    //return Array.from(this.content).filter(c => c.match(/[a-z]/i)).join("");
+    return (this.content.match(/[a-z]/gi) || []).join("");
+  }
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
   }
 }
 
-TranslatedPhrase.prototype = new Phrase();
+// // Defines a TranslatedPhrase object.
+// function TranslatedPhrase(content, translation) {
+//   this.content = content;
+//   this.translation = translation;
+//
+//  // Returns translation processed for palindrome testing.
+//   this.processedContent = function processedContent() {
+//     return this.processor(this.translation);
+//    }
+// }
+//
+// TranslatedPhrase.prototype = new Phrase();
